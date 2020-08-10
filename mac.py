6cc6bc8cfb6df636db0246ab2f2520ece4d34fa0
riccardomicheletto <riccardo.micheletto@studenti.unitn.it>
2020-08-10 16:04:43 +0200

    Base for ack timeout

--- mac.py
+++ mac.py
@@ -12,12 +12,13 @@ class Mac(object):
         self.latitude = self.node.latitude
         self.longitude = self.node.longitude
         self.phy = phy.Phy(self)
-        self.pendingPackets = []
+        self.pendingPackets = {}
 
     def send(self, destination, payloadLength, id):
         length = payloadLength + parameters.MIN_MAC_PKT_LENGTH
         macPkt = macPacket.MacPacket(self.name, destination, length, id, False)
         self.env.process(self.phy.send(macPkt))
+        self.pendingPackets[macPkt.id] = self.env.process(self.waitAck(macPkt.id))
 
     def handleReceivedPacket(self, macPkt):
         if macPkt.destination == self.name and not macPkt.ack:  # send ack to normal packets
@@ -27,3 +28,14 @@ class Mac(object):
             self.node.receive(macPkt.id, macPkt.source)
         elif macPkt.destination == self.name:
             print('Time %d: %s receives ACK %s from %s' % (self.env.now, self.name, macPkt.id, macPkt.source))
+            if macPkt.id in self.pendingPackets:    # packet could not be in pendingPackets if timeout has expired but ack still arrive
+                self.pendingPackets[macPkt.id].interrupt()
+
+    def waitAck(self, pktId):
+        try:
+            yield self.env.timeout(7000)
+            # timeout expired, resend
+            self.pendingPackets.pop(pktId)
+        except simpy.Interrupt:
+            # ack received
+            self.pendingPackets.pop(pktId)
